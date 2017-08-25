0357cfc40481d86849d4111b1eb38371b1bcef69
Tiff Willson <tiff@roomhints.com>
2017-08-25 14:32:54 -0700

    files

---
+++ aptzilla_/db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO `Apartments` (`address`, `price`, `bedroom`, `city`, `state`, `zip`, `lat`, `long`, `image_url`) 
+VALUES
+	('1725 Kearny St', '11500', '3', 'San Francisco', 'CA', '94133', '37.804007', '-122.406859', 'https://www.dropbox.com/s/o1npctt4ybth16s/apartment_5.png?dl=0'),
+	('88 Howard St', '3830', '1', 'San Francisco', 'CA', '94105', '37.773972', '-122.431297', 'https://www.dropbox.com/s/d6ny84bzrrmzpmv/apartment_2.png?dl=0'),
+	('333 Bush St', '6200', '1', 'San Francisco', 'CA', '94105', '37.7906', '122.4031', 'https://www.dropbox.com/s/lqw3t4b1ptmv1ow/apartment_3.png?dl=0'),
+	('737 Pine St', '3395', '1', 'San Francisco', 'CA', '94105', '37.773972', '-122.431297', 'https://www.dropbox.com/s/lqw3t4b1ptmv1ow/apartment_3.png?dl=0'),
+	('123 Mason St', '2600', '0', 'San Francisco', 'CA', '94133', '37.804007', '-122.431297', 'https://www.dropbox.com/s/lz5to7b7i56b76s/apartment_4.png?dl=0');
+
+
